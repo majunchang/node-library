3099f9d47e8b4b48fa96508863ef3fa9255b8437
majunchang <2471978285@qq.com>
2017-12-03 19:02:29 +0800

    分类总览  使用3d的柱状图进行可视化合统计

---
+++ vue-frontpage/src/components/highCharts/highCharts.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="highCharts-box">
+    <div class="highcharts-container" id='highcharts-container'></div>
+    <div id="sliders">
+      <table>
+        <tr>
+          <td>α 角（内旋转角）</td>
+          <td><input id="alpha" type="range" min="0" max="45" value="15"/> <span id="alpha-value" class="value"></span>
+          </td>
+        </tr>
+        <tr>
+          <td>β 角（外旋转角）</td>
+          <td><input id="beta" type="range" min="-45" max="45" value="15"/> <span id="beta-value" class="value"></span>
+          </td>
+        </tr>
+        <tr>
+          <td>深度</td>
+          <td><input id="depth" type="range" min="20" max="100" value="50"/> <span id="depth-value"
+                                                                                   class="value"></span></td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Highcharts from 'highcharts/highstock';
+  import HighchartsMore from 'highcharts/highcharts-more';
+  import HighchartsDrilldown from 'highcharts/modules/drilldown';
+  import Highcharts3D from 'highcharts/highcharts-3d';
+
+  HighchartsMore(Highcharts)
+  HighchartsDrilldown(Highcharts);
+  Highcharts3D(Highcharts);
+
+  export default {
+    props: ['styles'],
+    name: 'highcharts',
+    data() {
+      return {
+        chart: null
+      }
+    },
+    mounted() {
+//      this.initChart();
+    },
+    methods: {
+      initChart(options) {
+//
+        this.options = options;
+        this.highchartsContainer = document.getElementById('highcharts-container')
+        this.highchartsContainer.style.width = (this.styles.width || 800) + 'px';
+        this.highchartsContainer.style.height = (this.styles.height || 600) + 'px';
+        this.chart = new Highcharts.Chart(this.options);
+
+        var _this = this;
+        function showValues() {
+          $('#alpha-value').html(_this.options.chart.options3d.alpha);
+          $('#beta-value').html(_this.options.chart.options3d.beta);
+          $('#depth-value').html(_this.options.chart.options3d.depth);
+        }
+
+        // Activate the sliders
+        $('#sliders input').on('input change', function(){
+          _this.chart.options.chart.options3d[this.id] = this.value;
+          showValues();
+          _this.chart.redraw(false);
+        });
+        showValues();
+      }
+    }
+  }
+</script>
+
+<style>
+  .highCharts-box{
+    margin: 0 auto;
+  }
+  .highcharts-container {
+    width: 800px;
+    height: 600px;
+    margin: 0 auto;
+  }
+
+</style>
--- vue-frontpage/src/pages/overviewOfClassify/overviewOfClassify.vue
+++ vue-frontpage/src/pages/overviewOfClassify/overviewOfClassify.vue
@@ -1,33 +1,113 @@
 <template>
-
+  <div class="overviewOfClassify">
+    <highChartsComponent ref='highCharts' :options='options' :styles='styles'></highChartsComponent>
+  </div>
 </template>
 
 <script>
-  import Highcharts from 'highcharts/highstock';
-  import HighchartsMore from 'highcharts/highcharts-more';
-  import HighchartsDrilldown from 'highcharts/modules/drilldown';
-  import Highcharts3D from 'highcharts/highcharts-3d';
-  import Highmaps from 'highcharts/modules/map';
+  // 引入图表组件
+  import highChartsComponent from '../../components/highCharts/highCharts.vue'
+  import axios from 'axios'
 
-  HighchartsMore(Highcharts)
-  HighchartsDrilldown(Highcharts);
-  Highcharts3D(Highcharts);
-  Highmaps(Highcharts);
-
-    export default {
-        props: {},
-        components: {},
-        data() {
-            return {}
+  export default {
+    props: {},
+    components: {
+      highChartsComponent
+    },
+    data() {
+      return {
+        styles: {
+          width: 1000,
+          height: 800
         },
-        created() {
-
-        },
-        computed: {},
-        methods: {}
+        options: {}
+      }
+    },
+    created() {
+      this.initOptions()
+    },
+    computed: {},
+    methods: {
+      initOptions() {
+        axios({
+          method: 'get',
+          url: '/proxy/fullStack/overviewOfClassify'
+        }).then((res) => {
+          if (res.data.code === 0) {
+            var arr = res.data.result;
+            // 在这里对arr 进行类别去重
+            var newArr = [
+              {
+                count: 1,
+                category: '刘亦菲'
+              }
+            ]
+            for (var i = 0; i < arr.length; i++) {
+              var obj = {
+                count: 1,
+                category: arr[i].category
+              }
+              var flag = false;
+              for (var k = 0; k < newArr.length; k++) {
+                if (newArr[k].category.indexOf(obj.category) != -1) {
+                  newArr[k].count++;
+                  flag = true
+                  break;
+                }
+              }
+              if (!flag) {
+                newArr.push(obj);
+              }
+            }
+            // 在这里得到newArr
+            newArr.shift();
+            var nameArr = [];
+            var valueArr = [];
+            newArr.forEach((item, index) => {
+              nameArr.push(item.category);
+              valueArr.push(item.count)
+            })
+            console.log(nameArr);
+            console.log(valueArr);
+            this.options = {
+              chart: {
+                renderTo: 'highcharts-container',
+                type: 'column',
+                options3d: {
+                  enabled: true,
+                  alpha: 15,
+                  beta: 15,
+                  depth: 50,
+                  viewDistance: 25
+                }
+              },
+              title: {
+                text: '书籍分类预览3D图'
+              },
+              xAxis: {
+                categories: nameArr
+              },
+              subtitle: {
+                text: '可通过滑动下方滑块观看不同的角度'
+              },
+              plotOptions: {
+                column: {
+                  depth: 25
+                }
+              },
+              series: [{
+                name: '书籍',
+                data: valueArr
+              }]
+            };
+            this.$refs.highCharts.initChart(this.options);
+          }
+        })
+      }
     }
+  }
 </script>
 
-<style>
-
+<style scoped>
+  @import "overviewOfClassify.css";
 </style>
